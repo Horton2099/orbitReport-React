19b2ef2ac5b207aa10de1645a94d331b408723a4
Austin <kenjigw@gmail.com>
2023-08-11 11:40:15 -0500

    updated status

--- src/components/Table.js
+++ src/components/Table.js
@@ -19,7 +19,7 @@ const Table = ({ sat }) => {
                   <td>{data.name}</td>
                   <td>{data.type}</td>
                   <td>{data.launchDate}</td>
-                  <td>{data.orbitType}</td>
+                  <td>{`${data.operational === true ? "active" : "inactive"}`}</td>
                 </tr>
                   
                   
